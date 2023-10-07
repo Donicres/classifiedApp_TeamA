b176f3c9d309cbc905c72ef5027f7ac4d10631c1
golden <njukanggolden@gmail.com>
2023-10-07 23:25:58 +0300

    created products routes

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,30 +4,57 @@ const db = require("../config/db")
 
 
 
-router.post('/create', async(req, res) => {
-    
-}); //GOLDEN
+  
+  
+function queryPromise(sql,values=[]){
+    return new Promise((resolve,reject)=>{
+      db.query(sql,values,(error,result)=>{
+        if(error){
+          reject(error)
+        }else{
+          resolve(result)
+        }
+      })
+    });
+  }  
+  
 
-router.get('/product', async(req, res) => {
+router.post('/create', async(request, respond) => {
+    try{
+        const {category_id, title, price, paddress } = request.body;
+        if(!category_id || !title || !price || !paddress ){
+            respond.send("Enter values")
+        } 
+        const uservalues = [category_id,title,price,paddress];
+        const myquery = "INSERT INTO Products (category_id, title, price, paddress,created_on ) VALUES (?,?,?,?,now())"
+        const result = await queryPromise(myquery,uservalues)
+        respond.send("INSERTED OKAY")
+  
+  
+    }catch(err){
+        console.log(err)
+    }
+    
+    });//GOLDEN
+
+ router.get('/product', async(req, res) => {
   
 }); //YVETTE
 
 router.get('/product/:productID ', async(req, res) => {
   
-}); //VIVIAN
+ }); //VIVIAN
 
 
 router.put('/update/:productID ', async(req, res) => {
 
   
-}); //BAUDWIN
+ }); //BAUDWIN
 
 
-router.delete('/delete/:productID ', async(req, res) => {
+ router.delete('/delete/:productID ', async(req, res) => {
   
-}); //DONADONI
-
-
+ }); //DONADONI
 
 
 
